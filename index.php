f23f22e3c6df8d54e1946e0a52fc91fff549dfbf
helloribs <helloribs@gmail.com>
2022-03-10 16:31:04 -0600

    Add about section

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
     include($_SERVER['DOCUMENT_ROOT'].'/partials/common/_head.php');
     include($_SERVER['DOCUMENT_ROOT'].'/partials/landing/_hero.php');
+    include($_SERVER['DOCUMENT_ROOT'].'/partials/landing/_about.php');
     include($_SERVER['DOCUMENT_ROOT'].'/partials/common/_footer.php');
---
+++ partials/landing/_about.php
@@ -0,0 +1,28 @@
+<section id="landing__about">
+  <div class="l-main-wrapper">
+    <div class="wrapper">
+      <img
+        src="/assets/img/landing/img_karina-salazar-gonzalez.png"
+        alt="Karina Salazar González">
+      <div>
+        <h2>Sobre mí</h2>
+        <h1>¿Quién es <strong>Karina Salazar Gonzalez</strong>?</h1>
+        <p>
+          Nutrióloga mexicana con una maestria en nutrición clínica, especialista
+          en nutrición deportiva, ISAK Nivel 3, 3 ciclos olímpicos y la primer mujer
+          nutrióloga en asistir a unos juegos olímpicos como nutrióloga de la
+          delegación mexicana en Tokyo 2020, más de 10 años de experciencia con
+          más de 35 mil casos atendidos al 2022.
+        </p>
+        <p>
+          Soñadora y fiel creyente que México será un mejor país en terminos de
+          salud y alimentación.
+        </p>
+      </div>
+    </div>
+    <div class="contact-wrapper">
+      <img src="/assets/img/landing/img_ksg-olympics.png" alt="Juegos olímpicos">
+      <a href="#contacto" class="link">Contáctame</a>
+    </div>
+  </div>
+</section>

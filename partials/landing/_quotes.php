897a0fdb6fdcb614519b9bafc05e954052662adb
Raúl Amador <helloribs@gmail.com>
2022-04-15 17:00:32 -0500

    Add mobile quotes

--- partials/landing/_quotes.php
+++ partials/landing/_quotes.php
@@ -1,26 +1,50 @@
+<?php
+  $quotes = [
+    [
+      'text' => 'Life is like riding a bicycle. To keep your balance you must keep moving.',
+      'author' => 'Albert Einstein'
+    ],
+    [
+      'text' => 'Set your goals high, and don’t stop till you get there.',
+      'author' => 'Bo Jackson'
+    ],
+    [
+      'text' => 'Our potential is one thing. What we do with it is quite another.',
+      'author' => 'Angela Duckworth'
+    ]
+  ];
+?>
+
 <section class="landing__quotes">
-  <div class="magic-quote">
-    <div>
-      <span>
-        <h1>Life is like riding a bicycle. To keep your balance you must keep moving.</h1>
-        <h2>Albert Einstein</h2>
-      </span>
-    </div>
+  <div class="desktop-wrapper">
+    <?php
+      foreach($quotes as $quote) {
+        echo '
+          <div class="magic-quote">
+            <div>
+              <span>
+                <h1>'.$quote['text'].'</h1>
+                <h2>'.$quote['author'].'</h2>
+              </span>
+            </div>
+          </div>
+        ';
+      }
+      unset($quote);
+    ?>
   </div>
-  <div class="magic-quote">
-    <div>
-      <span>
-        <h1>Set your goals high, and don’t stop till you get there.</h1>
-        <h2>Bo Jackson</h2>
-      </span>
-    </div>
-  </div>
-  <div class="magic-quote">
-    <div>
-      <span>
-        <h1>Our potential is one thing. What we do with it is quite another.</h1>
-        <h2>Angela Duckworth</h2>
-      </span>
-    </div>
+  <div class="mobile-wrapper">
+    <?php
+      foreach($quotes as $quote) {
+        echo '
+          <div>
+            <h1>'.$quote['text'].'</h1>
+            <h2>'.$quote['author'].'</h2>
+          </div>
+        ';
+      };
+      unset($quote);
+      unset($quotes);
+    ?>
   </div>
 </section>

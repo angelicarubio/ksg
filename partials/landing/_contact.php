4dda12f184b38c84fb25a660c3fb83b2f4a80922
Víctor Martínez <victorandresmtz@gmail.com>
2023-07-26 13:29:36 -0600

    feat(app): use getform instead of php mailer

--- partials/landing/_contact.php
+++ partials/landing/_contact.php
@@ -2,7 +2,7 @@
   <div class="l-main-wrapper">
     <h2>Contacto</h2>
     <p>Desbloquea tu potencial</p>
-    <form id="contact-form" action="/contact_mailer.php" method="POST" class="col-wrapper">
+    <form id="contact-form" action="https://getform.io/f/7dc42e3f-aa8c-44b3-9d43-9936f1d22558" method="POST" class="col-wrapper">
       <div class="col-2">
         <label for="name">
           <input type="text" name="name" id="name" placeholder="Nombre." required>
